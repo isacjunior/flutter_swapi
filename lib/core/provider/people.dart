e829cb50bb3b07d496191196b5316c142783e22b
Isac <isac.junior@ifood.com.br>
2019-10-02 15:21:32 -0300

    remove search

--- lib/core/provider/people.dart
+++ lib/core/provider/people.dart
@@ -5,29 +5,30 @@ import 'package:movies/core/service/people.dart';
 class PeopleProvider with ChangeNotifier {
   People _people;
   bool _isLoading = false;
-  String search;
 
   bool get loading => _isLoading;
   People get people => _people;
 
-  void setLoading(bool boolean) {
+  void _setLoading(bool boolean) {
     _isLoading = boolean;
     notifyListeners();
   }
 
-  void setPeople(People people) {
+  void _setPeople(People people) {
     _people = people;
   }
 
-  void setSearch(String name) {
-    search = name;
-    notifyListeners();
+  Future getPeople(String id) async {
+    _setLoading(true);
+    People people = await PeopleService.getPeople(id);
+    _setPeople(people);
+    _setLoading(false);
   }
 
-  Future getPeople(String id) async {
-    setLoading(true);
-    People people = await PeopleService.getPeople(id);
-    setPeople(people);
-    setLoading(false);
+  Future getLukeSkyWalker() async {
+    _setLoading(true);
+    People people = await PeopleService.getPeople('1');
+    _setPeople(people);
+    _setLoading(false);
   }
 }
